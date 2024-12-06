2646fa500e55d372be38b6fc842a936cab9666ca
Baddogel <baddogel@gmail.com>
2024-12-06 22:44:39 +0300

    add comments

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
         # Рандомная пауза от 200 до 400 секунд
         pyautogui.PAUSE = random.randint(200, 400)
 
-        # Сдвиг курсора по оси x в пикселях
+        # Величина рандомного сдвига курсора по оси x в пикселях
         random_x = random.randint(-100, 100)
 
-        # Сдвиг курсора по оси y в пикселях
+        # Величина рандомного сдвига курсора по оси y в пикселях
         random_y = random.randint(-100, 100)
 
-        # Рандомная скорость сдвига курсора от 0,25 до 1 секунды
+        # Рандомная величина скорости сдвига курсора от 0,25 до 1 секунды
         random_duration = random.uniform(0.25, 1)
 
         pyautogui.moveRel(random_x, random_y, duration=random_duration)
